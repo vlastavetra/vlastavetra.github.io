eb74bc94dbe44919e4db9a30bd7388512a9bc9da
vlastavetra <vlastavetra@gmail.com>
2020-03-29 18:42:21 +0300

    learn cool ux popup
    learn cool ux popup

---
+++ script.js
@@ -0,0 +1,51 @@
+var link = document.querySelector(".login-link");
+var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".modal-close");
+var login = popup.querySelector("[name=login]");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("login");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+} catch(err) {
+    isStorageSupport = false
+};
+
+link.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    if (storage) {
+        login.value = storage;
+        password.focus();
+    } else {
+      login.focus();
+    }
+});
+
+close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit",  function(evt)  {
+    if ( !login.value || !password.value) {
+    evt.preventDefault();
+    console.log("Проблемность");
+    } else {
+      if (isStorageSupport) {
+      localStorage.setItem("login", login.value)
+      }
+    }
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        if (popup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            popup.classList.remove("modal-show"); 
+        }
+    }
+});
